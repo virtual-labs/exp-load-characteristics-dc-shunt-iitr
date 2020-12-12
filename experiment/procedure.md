3cd46c9ecefba1dd9fb0547a8b069e9a2a1f1715
priyanshi <priyanshi.a07@gmail.com>
2020-12-12 10:53:55 +0530

    add

--- experiment/procedure.md
+++ experiment/procedure.md
@@ -1,8 +1,25 @@
-This Experiment aims to make students familiar with frequently used electrical equipment. To perform the experiment :<br><br>
-Step 1: Click on name, symbol and function of respective equipments.<br>
-Step 2: Click on "Next".<br>
-Step 3: Drag the component/symbol from first row to the suitable second row box based on discription given.<br>
-Step 4: Click on name, symbol and function of respective equipments.<br>
-Step 5: Click on "Next".<br>
-Step 6: Drag the component/symbol from first row to the suitable second row box based on discription given.<br>
-Step 5: Click on "Submit".<br>
+Step 1: Make all the Correct Connections.<br><br> 
+ 1. Positive Terminal of MCB - Positive Terminal of A Meter-1. <br>
+ 2. Positive Terminal of MCB - Positive Termainal of V Meter-2.<br>
+ 3. Negative Terminal of MCB - Negative Terminal of V Meter-1. <br>
+ 4. Negative Terminal of MCB - A2 of Motor.<br>
+ 5. A2 - F2.<br>
+ 6. L - Negative Termianl of A Meter-1. <br>
+ 7. F - Z1.<br>
+ 8. A - A1.<br>
+ 9. L2 - A2 of Generator.<br>
+ 10. A2 of Generator - F2 of Generator.<br>
+ 11. F2 of Generator - Negative Termianl of V Meter-2. <br>
+ 12. Negative Terminal of A Meter-2 - Positive Terminal of V Meter-2. <br>
+ 13. L1 - Positive Terminal of V Meter-2. <br>
+ 14. Positive Terminal of A Meter-2 - A1 of Generator. <br>
+ 15. Positive Terminal of A Meter-2 - Z1 of Generator. <br>
+ <img src="images/C_correctCOnnections.PNG" alt="" height="400" width="700"><br>
+Step 2: Click on "Check" Button.<br>
+Step 3: After Correct Connections the Circuit will be turned ON.<br>
+Step 4: Select the No. of "Bulbs" from the Lamp load.<br>
+Step 5: Note Down the readings of the Ammeters & Voltmeters.<br>
+Step 6: Click "Add" Button to Add the values to the Observation Table.<br>
+Step 7: Add different values to the Table.<br>
+Step 8: Click on "Graph" Button to Create Graph.<br>
+
---
+++ experiment/simulation/script2.js
@@ -0,0 +1,363 @@
+// var isOn = false;
+
+// function changeBulb() {
+
+//     if (isOn == false) {
+//       isOn = true;
+//       document.getElementById('push1').src = 'assets/push2.png'
+//       count++;
+//     } else {
+//       isOn = true;
+//       document.getElementById('push1').src = 'assets/push1.png'
+//       count--;
+//     }
+
+//     // bulb1 = document.getElementById('push1');
+//     // if (bulb1.src = "assets/push1.png") {
+//     //   bulb1.src = "assets/push2.png"
+//     // } else {
+//     //   bulb1.src = "assets/push1.png"
+//     // }      
+//   }
+
+  // function changeBulb2() 
+  //   {
+
+  //   bulb2 = document.getElementById('push1_9')
+  //   if (bulb2.src = "assets/push1.png") 
+  //   {
+  //     bulb2.src = "assets/push2.png"
+  //   } 
+  //   else
+  //    {
+  //      bulb2.src = "assets/push1.png"
+  //    }
+  //   }
+
+  //   function changeBulb3() 
+  //   {
+
+  //   bulb3 = document.getElementById('push1_8')
+  //   if (bulb3.src = "assets/push1.png") 
+  //   {
+  //     bulb3.src = "assets/push2.png"
+  //   } 
+  //   else
+  //    {
+  //      bulb3.src = "assets/push1.png"
+  //    }
+  //   }
+
+  //   function changeBulb4() 
+  //   {
+
+  //   bulb4 = document.getElementById('push1_7')
+  //   if (bulb4.src = "assets/push1.png") 
+  //   {
+  //     bulb4.src = "assets/push2.png"
+  //   } 
+  //   else
+  //    {
+  //      bulb4.src = "assets/push1.png"
+  //    }
+  //   }
+
+  //   function changeBulb5() 
+  //   {
+
+  //   bulb5 = document.getElementById('push1_6')
+  //   if (bulb5.src = "assets/push1.png") 
+  //   {
+  //     bulb5.src = "assets/push2.png"
+  //   } 
+  //   else
+  //    {
+  //      bulb5.src = "assets/push1.png"
+  //    }
+  //   }
+
+  //   function changeBulb6() 
+  //   {
+
+  //   bulb6 = document.getElementById('push1_5')
+  //   if (bulb6.src = "assets/push1.png") 
+  //   {
+  //     bulb6.src = "assets/push2.png"
+  //   } 
+  //   else
+  //    {
+  //      bulb6.src = "assets/push1.png"
+  //    }
+  //   }
+
+  //   function changeBulb7() 
+  //   {
+
+  //   bulb7 = document.getElementById('push1_4')
+  //   if (bulb7.src = "assets/push1.png") 
+  //   {
+  //     bulb7.src = "assets/push2.png"
+  //   } 
+  //   else
+  //    {
+  //      bulb7.src = "assets/push1.png"
+  //    }
+  //   }
+  //   function changeBulb8() 
+  //   {
+
+  //   bulb8 = document.getElementById('push1_3')
+  //   if (bulb8.src = "assets/push1.png") 
+  //   {
+  //     bulb8.src = "assets/push2.png"
+  //   } 
+  //   else
+  //    {
+  //      bulb8.src = "assets/push1.png"
+  //    }
+  //   }
+
+  //   function changeBulb9() 
+  //   {
+
+  //   bulb9 = document.getElementById('push1_2')
+  //   if (bulb9.src = "assets/push1.png") 
+  //   {
+  //     bulb9.src = "assets/push2.png"
+  //   } 
+  //   else
+  //    {
+  //      bulb9.src = "assets/push1.png"
+  //    }
+  //   }
+    
+  //   function changeBulb10() 
+  //   {
+  //   isOn = true;
+
+  //   if (isOn == true) {
+  //     bulb10 = document.getElementById('push1_1')
+  //     if (bulb10.src = "assets/push1.png") 
+  //     {
+  //       bulb10.src = "assets/push2.png"
+  //     } 
+  //   }
+    
+  //   else
+  //    {
+  //      bulb10.src = "assets/push1.png"
+  //      count--;
+  //    }
+  //   }
+
+//     var rangeClock =  document.querySelector('#niddle_5');
+//     var rangeClock2 =  document.querySelector('#niddle_4');
+//     var rangeClock3 =  document.querySelector('#niddle_2');
+//     var rangeClock4 =  document.querySelector('#niddle_3');
+//     var A1 = [3,3.6,5.4,6.8,8,10,11.5,13,14.2,15.2];
+//     var V1 = [225,225,225,225,225,225,225,225,225,225];
+//     var A2 = [0,2.8,3.2,3.6,5.5,7,8.1,10.2,11,12.7];
+//     var V2 = [220,212,208,205,200,195,189,184,179,176];
+    
+    
+//     function rangeChange1() {
+       
+//         // rangeShow.value = rotateClock;
+//         rangeClock.style.transform = 'rotate(-47.5deg)';
+//         rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//         rangeClock3.style.transform = 'rotate(-70deg)';
+//         rangeClock4.style.transform = 'rotateZ(76.67deg)';
+//         // rangeShow.value = rotateClock;
+//     }
+
+//     function rangeChange2() {
+     
+//       // rangeShow.value = rotateClock;
+//       rangeClock.style.transform = 'rotate(-43deg)';
+//       rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//       rangeClock3.style.transform = 'rotate(-49deg)';
+//       rangeClock4.style.transform = 'rotateZ(71.33deg)';
+//       // rangeShow.value = rotateClock;
+//   }
+
+//   function rangeChange3() {
+     
+//     // rangeShow.value = rotateClock;
+//     rangeClock.style.transform = 'rotate(-29.5deg)';
+//     rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//     rangeClock3.style.transform = 'rotate(-41.5deg)';
+//     rangeClock4.style.transform = 'rotateZ(68.66deg)';
+//     // rangeShow.value = rotateClock;
+//   }
+
+//   function rangeChange4() {
+      
+//     // rangeShow.value = rotateClock;
+//     rangeClock.style.transform = 'rotate(-19deg)';
+//     rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//     rangeClock3.style.transform = 'rotate(-43deg)';
+//     rangeClock4.style.transform = 'rotateZ(66.66deg)';
+//     // rangeShow.value = rotateClock;
+//   }
+
+//   function rangeChange5() {
+     
+//     // rangeShow.value = rotateClock;
+//     rangeClock.style.transform = 'rotate(-10deg)';
+//     rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//     rangeClock3.style.transform = 'rotate(-28.75deg)';
+//     rangeClock4.style.transform = 'rotateZ(63.33deg)';
+//     // rangeShow.value = rotateClock;
+// }
+
+// function rangeChange6() {
+     
+//   // rangeShow.value = rotateClock;
+//   rangeClock.style.transform = 'rotate(5deg)';
+//   rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//   rangeClock3.style.transform = 'rotate(-17.5deg)';
+//   rangeClock4.style.transform = 'rotateZ(60deg)';
+//   // rangeShow.value = rotateClock;
+// }
+
+// function rangeChange7() {
+     
+//   // rangeShow.value = rotateClock;
+//   rangeClock.style.transform = 'rotate(16.25deg)';
+//   rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//   rangeClock3.style.transform = 'rotate(-9.25deg)';
+//   rangeClock4.style.transform = 'rotateZ(56deg)';
+//   // rangeShow.value = rotateClock;
+// }
+
+// function rangeChange8() {
+     
+//   // rangeShow.value = rotateClock;
+//   rangeClock.style.transform = 'rotate(27.5deg)';
+//   rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//   rangeClock3.style.transform = 'rotate(6.5deg)';
+//   rangeClock4.style.transform = 'rotateZ(52.67deg)';
+//   // rangeShow.value = rotateClock;
+// }
+
+// function rangeChange9() {
+     
+//   // rangeShow.value = rotateClock;
+//   rangeClock.style.transform = 'rotate(36.5deg)';
+//   rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//   rangeClock3.style.transform = 'rotate(12.5deg)';
+//   rangeClock4.style.transform = 'rotateZ(49.33deg)';
+//   // rangeShow.value = rotateClock;
+// }
+
+// function rangeChange10() {
+     
+//   // rangeShow.value = rotateClock;
+//   rangeClock.style.transform = 'rotate(44deg)';
+//   rangeClock2.style.transform = 'rotateZ(79.95deg)';
+//   rangeClock3.style.transform = 'rotate(25.25deg)';
+//   rangeClock4.style.transform = 'rotateZ(47.33deg)';
+//   // rangeShow.value = rotateClock;
+// }
+// var count = 0;
+// function bulbClick() {
+  
+
+//   // count++;
+  
+//   if (count==1) {
+    
+//     rangeChange1();
+//   }
+//   if (count==2) {
+//     rangeChange2();
+//   }
+//   if (count==3) {
+//     rangeChange3();
+//   }
+//   if (count==4) {
+//     rangeChange4();
+//   }
+//   if (count==5) {
+//     rangeChange5();
+//   }
+//   if (count==6) {
+//     rangeChange6();
+//   }
+//   if (count==7) {
+//     rangeChange7();
+//   }
+//   if (count==8) {
+//     rangeChange8();
+//   }
+//   if (count==9) {
+//     rangeChange9();
+//   }
+//   if (count==10) {
+//     rangeChange10();
+//   }
+
+// }
+
+
+
+// rangeClock.addEventListener('click', rangeChange);
+
+// var table = document.querySelector('table');
+// var addToTable = document.getElementById('addToTable');
+// var graphPage = document.getElementById('button_3');
+// var graph = document.getElementById('myButton');
+// var x = table.insertRow(0);
+
+// var cell1 = x.insertCell(0);
+// var cell2 = x.insertCell(1);
+
+// cell1.innerHTML = "Load Current";
+// cell2.innerHTML = "Terminal Voltage";
+
+// var trace1 = {
+//   x: [],
+//   y: [],
+//   type: 'scatter'
+// };
+
+// var cellX;
+// var cellY;
+
+// addToTable.addEventListener('click', () => {
+//     var y = table.insertRow(1);
+
+//     cell1.innerHTML = "Load Current";
+//     cell2.innerHTML = "Terminal Voltage";
+//     cellX = y.insertCell(0);
+//     cellY = y.insertCell(1);
+
+//     for (let i = 0; i < 10; i++) {
+//       if (count == (i+1)) {
+//           cellX.innerHTML = A2[i];
+//           cellY.innerHTML = V2[i];
+//       }      
+//     }
+//     trace1.x.push(cellX.innerHTML);
+//     trace1.y.push(cellY.innerHTML); 
+
+//     // var data = [trace1];
+//     // Plotly.newPlot('myDiv', data, {}, {showSendToCloud: true}); 
+//   })
+
+//   graphPage.addEventListener('click', () => {
+//     var data = [trace1];
+//     Plotly.newPlot('myDiv', data, {}, {showSendToCloud: true}); 
+//     // console.log(graphPage);
+    
+
+//   })
+
+
+
+  
+
+
+
+
+
